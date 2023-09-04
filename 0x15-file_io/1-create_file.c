919af7372ce527d8f0e784c9de9778637b0a29bb
nouhask <nouhaylaskilat@gmail.com>
2023-09-04 11:55:34 -0700

    io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	ujd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	ljmx = write(ujd, text_content, len);
 
-	if (ujd == -1 || lmx == -1)
+	if (ujd == -1 || ljmx == -1)
 		return (-1);
 
 	close(ujd);
