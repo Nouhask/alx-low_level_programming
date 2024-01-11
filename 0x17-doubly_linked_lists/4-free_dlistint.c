a224d3e2ce6438b1f33beed26fce983c6ac91aeb
nouhask <nouhaylaskilat@gmail.com>
2024-01-11 06:04:12 -0800

    c dlists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,15 +8,15 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp;
+	dlistint_t *free_list;
 
 	if (head != NULL)
 		while (head->prev != NULL)
 			head = head->prev;
 
-	while ((tmp = head) != NULL)
+	while ((free_list = head) != NULL)
 	{
 		head = head->next;
-		free(tmp);
+		free(free_list);
 	}
 }
